e3000f825e2d8cebfcdb5fb3ab25ffb5add15ccf
Mario <Maslz980@outlook.com>
2017-03-17 15:46:05 -0600

    Haciendo main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Bender.h"
+#include "Airbenders.h"
+#include "Earthbenders.h"
+#include "Firebenders.h"
+#include "Waterbenders.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
